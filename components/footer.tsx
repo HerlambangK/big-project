50164941b7d1c2c8fb91bcc315dfd3648b1726c1
herlambang k <herlambangk25@gmail.com>
2023-07-21 22:45:47 +0700

    add fiture all

--- components/footer.tsx
+++ components/footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
   return (
     <footer className="mt-20 bg-white dark:bg-gray-800">
       <div className="container w-full p-4 md:flex md:items-center md:justify-between md:p-6">
-        <span className="text-sm text-gray-500 dark:text-gray-400 sm:text-center">© 2023 <a href="https://flowbite.com/" className="hover:underline">Bikinsoal™</a>. All Rights Reserved.
+        <span className="text-sm text-gray-500 dark:text-gray-400 sm:text-center">© 2023 <a href="https://flowbite.com/" className="hover:underline">Herlambang &Co</a>. All Rights Reserved.
         </span>
         <ul className="mt-3 flex flex-wrap items-center text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -36,13 +36,16 @@ const Navbar = () => {
     <SessionProvider >
       <div className={cn("fixed inset-x-0 top-0 z-10 flex justify-between bg-white p-4 transition lg:px-12", isScrolled ? 'shadow-sm' : '')}>
         <Link href="/" className='inline-flex'>
-          <Image alt='logo' src={"/logo.png"} width={175} height={100} className="w-24 object-contain lg:w-32 xl:w-40" />
+          <Image alt='logo' src={"/Niagaweb-logo.png"} width={175} height={100} className="w-24 object-contain lg:w-32 xl:w-40" />
         </Link>
 
         <div className='hidden items-center gap-4 md:inline-flex'>
-          <Link href="/generate" className={cn('rounded-lg px-3 py-2 text-sm font-semibold hover:bg-zinc-100', pathName === '/generate' && 'bg-zinc-100')}>Generate Soal</Link>
-          <Link href="/banksoal" className={cn('rounded-lg px-3 py-2 text-sm font-semibold hover:bg-zinc-100', pathName === '/banksoal' && 'bg-zinc-100')}>Bank Soal</Link>
+          <Link href="/generate" className={cn('rounded-lg px-3 py-2 text-sm font-semibold hover:bg-zinc-100', pathName === '/generate' && 'bg-zinc-100')}>About</Link>
+          <Link href="/banksoal" className={cn('rounded-lg px-3 py-2 text-sm font-semibold hover:bg-zinc-100', pathName === '/banksoal' && 'bg-zinc-100')}>Portofolio</Link>
           <Link href="/myaccount" className={cn('rounded-lg px-3 py-2 text-sm font-semibold hover:bg-zinc-100', pathName === '/myaccount' && 'bg-zinc-100')}>Akun Saya</Link>
+          {/* <Link href="/generate" className={cn('rounded-lg px-3 py-2 text-sm font-semibold hover:bg-zinc-100', pathName === '/generate' && 'bg-zinc-100')}>Generate Soal</Link>
+          <Link href="/banksoal" className={cn('rounded-lg px-3 py-2 text-sm font-semibold hover:bg-zinc-100', pathName === '/banksoal' && 'bg-zinc-100')}>Bank Soal</Link>
+          <Link href="/myaccount" className={cn('rounded-lg px-3 py-2 text-sm font-semibold hover:bg-zinc-100', pathName === '/myaccount' && 'bg-zinc-100')}>Akun Saya</Link> */}
           <ButtonSession />
         </div>
         <div className='flex md:hidden'>
